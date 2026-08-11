b2d4ba8ab1c9392abb6680a252a22fce64aa0a29
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2026-08-12 01:02:04 +0300

    init

---
+++ src/main/java/com/matvey/innowisepaymentservice/InnowisePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.matvey.innowisepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowisePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowisePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matvey/innowisepaymentservice/InnowisePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matvey.innowisepaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowisePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
